bea0c87716e9219fc337144ace19c04cd73aec5a
Yas-Shen <ylshenair@outlook.com>
2023-02-09 16:55:03 +0000

    only one more test but I'm done

--- functions.php
+++ functions.php
@@ -28,13 +28,19 @@ function displayVinyl(array $vinylCollection): string
 {
     $result = '<div class="container">';
     foreach ($vinylCollection as $vinyl) {
-        $result .= '<div class="items">' .
-            '<img src="' . $vinyl['image'] . '"/>'.'<br>' .
-            '<h1>' . $vinyl['artist'] . '</h1>' . '<br>' .
-            '<h3>' . $vinyl['album'] . '</h3>' . '<br>' .
-            '<p class="year">' . $vinyl['year'] . '</p>' . '<br>' .
-            '<p class="genre">' . $vinyl['genre'] . '</p>' . '<br>' .
-            '<p class="label">' . $vinyl['label-released'] . '</p>' . '</div>';
+        if (array_key_exists('image', $vinyl) && array_key_exists('artist', $vinyl)
+            && array_key_exists('album', $vinyl)
+        && array_key_exists('year', $vinyl)
+        && array_key_exists('genre', $vinyl)
+        && array_key_exists('label-released', $vinyl)) {
+            $result .= '<div class="items">' .
+                '<img src="' . $vinyl['image'] . '"/>' . '<br>' .
+                '<h1>' . $vinyl['artist'] . '</h1>' . '<br>' .
+                '<h3>' . $vinyl['album'] . '</h3>' . '<br>' .
+                '<p class="year">' . $vinyl['year'] . '</p>' . '<br>' .
+                '<p class="genre">' . $vinyl['genre'] . '</p>' . '<br>' .
+                '<p class="label">' . $vinyl['label-released'] . '</p>' . '</div>';
+        }
     }
     $result .= "</div>";
     return $result;
